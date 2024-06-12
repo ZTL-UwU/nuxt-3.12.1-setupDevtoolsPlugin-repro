a755dc43c1e99ce332f70ccc8b119b6ced0e67a8
ZTL-UwU <zhangtianli2006@163.com>
2024-06-12 13:16:48 +0800

    chore: init
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <VeeForm>
+    <VeeField name="email" type="email" :rules="fieldSchema" />
+    <VeeErrorMessage name="email" />
+  </VeeForm>
+</template>
+
+<script setup>
+import { toTypedSchema } from '@vee-validate/zod';
+import * as zod from 'zod';
+const fieldSchema = toTypedSchema(
+  zod.string().nonempty('Field is required').email('Must be a valid email')
+);
+</script>
